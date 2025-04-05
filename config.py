eb49e7add296598c464da7e858b43ad4ffc7e48e
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-04-05 11:25:29 +0600

    Update Config For New Variables

--- config.py
+++ config.py
@@ -2,3 +2,5 @@
 API_ID = "282xx710"  # Replace with your API ID Get From my.telegram.org
 API_HASH = "7fc5b3ddghsdxxxb86481ab5eca3"  # Replace with your API Hash Get From my.telegram.org
 BOT_TOKEN = "78249889xxxxffergipu88wmA9u1tOtq8qddzhyL0"  # Replace with your Bot Token Get From @BotFather 
+COMMAND_PREFIX = ["/", ".", "!", ","]    #Used Just For Proffessionalism In Bot 
+PROFILE_ERROR_URL = "https://t.me/BotsDevZone/243"  #Replace With Your Desired One 
